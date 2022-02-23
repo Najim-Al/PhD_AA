f9a2af1ef39d867db1e323af65b580f8b48559b7
lixudong <lee@algolabs.com>
2022-02-23 20:52:50 +0000

    add fitness_func_pnl

--- AgentsGA.py
+++ AgentsGA.py
@@ -3,7 +3,11 @@ import pandas as pd
 from geneticalgorithm import geneticalgorithm as ga
 from hedge_project import *
 
-data_datesum = pd.read_pickle('data.obj')
+
+import pickle5 as pickle
+with open('data.obj', "rb") as fh:
+  data_datesum = pickle.load(fh)
+
 model_param = []
 
 
@@ -14,9 +18,10 @@ algorithm_param = {'max_num_iteration': 100,\
                    'crossover_probability': 0.5,\
                    'parents_portion': 0.3,\
                    'crossover_type':'uniform',\
+                   'function_timeout ':60,
                    'max_iteration_without_improv':None}
 
-varbound=np.array([[0,100], #limits
+varbound=np.array([[0,1e8], #limits
                    [-1,0], #fractions
                    [0,1], #skew
                    [-1440,1440]  #window
@@ -24,6 +29,42 @@ varbound=np.array([[0,100], #limits
 
 #varibal type
 vartype=np.array([['int'],['real'],['real'],['int']])
+
+
+def fitness_func_pnl(X):
+    limit,fraction,skew,window = int(X[0]),X[1],X[2],int(X[3])
+    ind = indicator(data_datesum['mid_price'].values, window)
+    agent = Agent(limits=limit,hedge_fractions=fraction,skew = skew,indicator=ind,window= window)
+    netpos = data_datesum['NetPosUsd'].values
+    hedges = []
+    for index, nop in enumerate(netpos):
+        hedges.append(agent.Action(nop, index))
+
+    results = np.array(hedges)
+    pnl_result = (results)*data_datesum['NetUsdPnL']
+    root_pnl = pnl_result+data_datesum['NetUsdPnL']
+
+    # print(data_datesum['NetUsdPnL'].sum())
+
+    hedge = pnl_result.sum()
+    print(hedge)
+
+
+    root = root_pnl.sum()
+    # print(root)
+
+    cumpnl = root_pnl.cumsum()
+    meanDD = (np.maximum.accumulate(cumpnl) - cumpnl).mean()
+    model_param.append({
+        'PnL':root,
+        'MeanDD':meanDD,
+        'param': f'limits:{limit}, hedge_fractions:{fraction},skew:{skew},window:{window}'
+
+    })
+    print(f"'PnL':{root},'MaxDD':{meanDD},'limits"
+          f":{limit}, hedge_fractions:{fraction},skew:{skew},window:{window}'")
+
+    return -1.0 * hedge
 
 def fitness_func(X):
     limit,fraction,skew,window = int(X[0]),X[1],X[2],int(X[3])
@@ -36,8 +77,12 @@ def fitness_func(X):
 
     results = np.array(hedges)
     pnl_result = results*data_datesum['NetUsdPnL'].values
+    print((results*data_datesum['NetUsdPnL']).sum())
+
     pnl = pnl_result.sum()
-    cumpnl = results.cumsum()
+    print(pnl)
+
+    cumpnl = pnl_result.cumsum()
     maxDD = (np.maximum.accumulate(cumpnl) - cumpnl).max()
     model_param.append({
         'PnL':pnl,
@@ -51,7 +96,7 @@ def fitness_func(X):
 # res = fitness_func(np.array([20,-0.2,0.2,40]))
 # print(res)
 
-model=ga(function=fitness_func,dimension=4,variable_type_mixed=vartype,variable_boundaries=varbound,algorithm_parameters=algorithm_param)
+model=ga(function=fitness_func_pnl,dimension=4,variable_type_mixed=vartype,variable_boundaries=varbound,algorithm_parameters=algorithm_param)
 
 model.run()
 model_result = pd.DataFrame.from_dict(data=model_param)
